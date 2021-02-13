70a72ffa712a57b9d25b800f74177245aeb29834
Francisco Zapata <drfcozapata@gmail.com>
2021-02-13 01:03:30 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/**
+ * @author DrFcoZapata
+ */
+
+let menu = document.querySelector('#menu');
+let menu_bar = document.querySelector('#menu-bar');
+
+menu_bar.addEventListener('click', function() {
+    menu.classList.toggle('menu-toggle');
+})
